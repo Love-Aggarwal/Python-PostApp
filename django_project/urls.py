543003eb5c347d723ad699268add0fb86ae12ee9
Love-Aggarwal <47328559+Love-Aggarwal@users.noreply.github.com>
2021-04-02 18:26:08 +0530

    Update urls.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -23,7 +23,6 @@ urlpatterns = [
     path('',include('blog.urls')),
     path('cadmin/',include('cadmin.urls')),
     url(r'^media/(?P<path>.*)$',serve,{'document_root': settings.MEDIA_ROOT} ),
-    url(r'^static/(?P<path>.*)$',serve,{'document_root': settings.STATIC_ROOT} ),
     
 
     # url(r'^category/(?P<category_slug>[\w-]+)/$', views.post_by_category, name='post_by_category'),
